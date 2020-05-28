5505069226fff8fa2362c642ec74a5f5cedf5a55
laionazeredo <laion.azeredo@gmail.com>
2020-05-27 23:43:35 -0300

    -v 1.1.0 (Bugs corrected and some Ui improvements)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-var Tesseract = window.Tesseract;
+var Tesseract = require('tesseract.js');
 
 class App extends Component {
   
@@ -34,35 +34,42 @@ class App extends Component {
     }
   }
 
-  // This method reads the uploads, call Tesseract API and generates the text.
+  // This method reads the uploads, call Tesseract API and generates the result text, the pattern and confidence level of prediction.
   generateText = () => {
     let uploads = this.state.uploads
   
     for(var i = 0; i < uploads.length; i++) {
-      Tesseract.recognize(uploads[i], {
-        lang: 'eng'
-      })
+      Tesseract.recognize(uploads[i],'eng')
       .catch(err => {
         console.error(err)
       })
       .then(result => {
         // Get Confidence score
-        let confidence = result.confidence
+        let confidence =  result.data.confidence
+        
+        // Define a confidence Flag to help the user guess if the result is good or not
+        let confidenceFlag = ""
   
         // Get full output
-        let text = result.text
+        let text = result.data.text
   
         // Get codes
-        let pattern = /\b\w{10,10}\b/g
-        let patterns = result.text.match(pattern);
-  
+        let pattern =  /./g;
+        let patterns = result.data.text.match(pattern);
+        
+        // Set confidence flag
+        if (confidence < 50) {confidenceFlag = "Poor"};
+        if (confidence > 50 && confidence < 90) {confidenceFlag = "Good"};
+        if (confidence > 90) {confidenceFlag = "Very Good!"};
+        
         // Update state
         this.setState({ 
           patterns: this.state.patterns.concat(patterns),
           documents: this.state.documents.concat({
             pattern: patterns,
             text: text,
-            confidence: confidence
+            confidence: confidence,
+            confidenceFlag: confidenceFlag
           })
         })
       })
@@ -72,12 +79,17 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        
         <header className="header">
-          <h1>TesseReact - The OCR app made with React and Tesseract</h1>
+          
+          <h1>TesseReact</h1>
+          <h2> The OCR app made with React and Tesseract</h2>
+        
         </header>
 
-        { /* File uploader */ }
+        { /* File uploader  Input*/ }
         <section className="hero">
+          
           <label className="fileUploaderContainer">
             Click here to upload documents
             <input type="file" id="fileUploader" onChange={this.handleChange} multiple />
@@ -89,28 +101,36 @@ class App extends Component {
             }) }
           </div>
 
-          <button onClick={this.generateText} className="button">Generate</button>
+          <button onClick={this.generateText} className="button">Read the Image Now!</button>
         </section>
 
-        { /* Results */ }
+        { /* Results Area */ }
         <section className="results">
           { this.state.documents.map((value, index) => {
             return (
+              
               <div key={index} className="results__result">
+                
                 <div className="results__result__image">
                   <img src={this.state.uploads[index]} width="250px" />
                 </div>
+                
                 <div className="results__result__info">
+                  
                   <div className="results__result__info__codes">
-                    <small><strong>Confidence Score:</strong> {value.confidence}</small>
+                    <small><strong>Your Confidence Score is:</strong> {value.confidence} ({value.confidenceFlag})</small>
                   </div>
+                  
                   <div className="results__result__info__codes">
-                    <small><strong>Pattern Output:</strong> {value.pattern.map((pattern) => { return pattern + ', ' })}</small>
+                    <small><strong>The Pattern Identified is:</strong> {value.pattern.map((pattern) => { return pattern + ', ' })}</small>
                   </div>
+                  
                   <div className="results__result__info__text">
-                    <small><strong>Full Output:</strong> {value.text}</small>
+                    <small><strong>Here is the Full Output:</strong> {value.text}</small>
                   </div>
+                
                 </div>
+              
               </div>
             )
           }) }
